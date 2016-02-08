f674dc11f132aa3c8ebf1936509c5761c7ad3407
Dashamir Hoxha <dashohoxha@gmail.com>
2016-02-08 08:41:42 +0100

    Test archive auto init.

---
+++ tests/t0004-auto-init.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+test_description='If an archive does not exist, it is created automatically.'
+source "$(dirname "$0")"/setup.sh
+
+test_expect_success 'A new archive is created if it does not exist.' '
+    [[ ! -e "$PW_DIR/pw.tgz.gpg" ]] &&
+    echo -e "$PASSPHRASE\n$PASSPHRASE\n" | "$PW" ls | grep "Creating a new archive " &&
+    [[ -e "$PW_DIR/config.sh" ]] &&
+    [[ -e "$PW_DIR/pw.tgz.gpg" ]] &&
+    "$PW" ls <<< "$PASSPHRASE"
+'
+
+test_expect_success 'A given archive is created if it does not exist.' '
+    [[ ! -e "$PW_DIR/test1.tgz.gpg" ]] &&
+    echo -e "$PASSPHRASE\n$PASSPHRASE\n" | "$PW" -a test1 ls | grep "Creating a new archive " &&
+    [[ -e "$PW_DIR/test1.tgz.gpg" ]] &&
+    "$PW" -a test1 ls <<< "$PASSPHRASE"
+'
+
+test_expect_success 'Another archive is created if it does not exist.' '
+    [[ ! -e "$PW_DIR/test2.tgz.gpg" ]] &&
+    echo -e "$PASSPHRASE\n$PASSPHRASE\n" | "$PW" -a test2 ls | grep "Creating a new archive " &&
+    [[ -e "$PW_DIR/test2.tgz.gpg" ]] &&
+    "$PW" -a test2 ls <<< "$PASSPHRASE"
+'
+
+test_done
