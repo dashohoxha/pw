34bb02760c387fe749881f88c3372c19c386176e
Dashamir Hoxha <dashohoxha@gmail.com>
2016-02-09 11:02:42 +0100

    More tests for set, ls, show.

--- tests/setup-02.sh
+++ tests/setup-02.sh
@@ -1,3 +1,6 @@
 source "$(dirname "$0")"/setup-01.sh
 
-PASSPHRASE='0123456789'
+PASSPHRASE='123'
+PASS1='01'
+PASS2='02'
+PASS3='03'
--- tests/t0010-set-ls-show.sh
+++ tests/t0010-set-ls-show.sh
@@ -4,11 +4,20 @@ test_description='Test commands: set, ls, show'
 source "$(dirname "$0")"/setup-03.sh
 
 test_expect_success 'Set a new password.' '
-    local pass1="xyz" &&
-    echo -e "$PASSPHRASE\n$pass1\n$pass1" | "$PW" set test1 &&
+    echo -e "$PASSPHRASE\n$PASS1\n$PASS1" | "$PW" set test1 &&
     echo -e "$PASSPHRASE" | "$PW" ls | grep "test1" &&
-    local pass2=$("$PW" show test1 <<<"$PASSPHRASE") &&
-    [[ $pass1 == $pass2 ]]
+    local pass1=$("$PW" show test1 <<<"$PASSPHRASE") &&
+    [[ $PASS1 == $pass1 ]]
+'
+
+test_expect_success 'Check that password retype matches.' '
+    local pass1="x-$PASS1" &&
+    echo -e "$PASSPHRASE\n$PASS1\n$pass1" | "$PW" set test2 | grep "Error: the entered passwords do not match."
+'
+
+test_expect_success 'Check that ls can show the password.' '
+    local pass1=$("$PW" ls test1 <<<"$PASSPHRASE") &&
+    [[ $PASS1 == $pass1 ]]
 '
 
 test_done
