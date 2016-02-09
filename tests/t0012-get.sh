c89dbdcec31d1d0e7cb8081c3c440a9cc511afb2
Dashamir Hoxha <dashohoxha@gmail.com>
2016-02-09 11:27:29 +0100

    Test get.

---
+++ tests/t0012-get.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+test_description='Test commands: set, ls, show'
+source "$(dirname "$0")"/setup-03.sh
+
+test_expect_success 'Set a new password.' '
+    echo -e "$PASSPHRASE\n$PASS1\n$PASS1" | "$PW" set test1
+'
+
+test_expect_success 'Test get.' '
+    "$PW" get test1 <<<"$PASSPHRASE" &&
+    local pass1=$(xclip -selection clipboard -o) &&
+    [[ $PASS1 == $pass1 ]]
+'
+
+test_expect_success 'Test that get is the default command.' '
+    "$PW" test1 <<<"$PASSPHRASE" &&
+    local pass1=$(xclip -selection clipboard -o) &&
+    [[ $PASS1 == $pass1 ]]
+'
+
+test_done
