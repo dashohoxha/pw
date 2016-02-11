6898697f64a24b3fd78a7f418da0fbfe12c3088d
Dashamir Hoxha <dashohoxha@gmail.com>
2016-02-11 21:30:01 +0100

    Test rm

---
+++ tests/t0043-rm.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+test_description='Test command rm'
+source "$(dirname "$0")"/setup-04.sh
+
+test_expect_success 'Test rm' '
+    [[ $(pwp show test1) == "$PASS1" ]] &&
+    pw rm test1 <<-_EOF &&
+$PASSPHRASE
+n
+_EOF
+    [[ $(pwp show test1) == "$PASS1" ]]
+
+    pw rm test1 <<-_EOF &&
+$PASSPHRASE
+y
+_EOF
+    pwp show test1 | grep "Error: test1 is not in the password store." &&
+    pwp rm test1 | grep "Error: test1 is not in the password store."
+'
+
+test_expect_success 'Test rm -f' '
+    [[ $(pwp show test2/test4) == "$PASS2" ]] &&
+    pwp rm test2/test4 -f &&
+    pwp show test2/test4 | grep "Error: test2/test4 is not in the password store."
+'
+
+test_expect_success 'Test rm -r' '
+    pwp rm test2 -r -f &&
+    pwp show test2/test5 | grep "Error: test2/test5 is not in the password store."
+'
+
+test_done
