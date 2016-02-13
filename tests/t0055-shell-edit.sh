191a44981cbc1ca5eabd7e404a402ae271d7a718
Dashamir Hoxha <dashohoxha@gmail.com>
2016-02-13 21:58:56 +0100

    Test shell find

--- tests/t0055-shell-edit.sh
+++ tests/t0055-shell-edit.sh
@@ -3,6 +3,10 @@
 test_description='Test shell edit'
 source "$(dirname "$0")"/setup-05.sh
 
+test_expect_success 'Test edit usage' '
+    run_in_shell "edit" | grep "Usage: edit "
+'
+
 test_expect_success 'Test edit.' '
     export EDITOR=ed &&
     run_in_shell "edit test2/test3" "1c" "$PASS2" "." "wq" &&
