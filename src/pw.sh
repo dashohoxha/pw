c62d311ac2ef6804c8089bb4362cf9bbb5796f2b
Dashamir Hoxha <dashohoxha@gmail.com>
2016-02-19 16:59:10 +0100

    Make cmd_gen external (the main benefit is that it can be overriden by a custom stcript placed at /ext/)

--- src/pw.sh
+++ src/pw.sh
@@ -436,58 +436,6 @@ cmd_edit() {
     archive_lock      # cleanup $WORKDIR
 }
 
-cmd_generate() {
-    local opts force=0 symbols="-y" inplace=0
-    opts="$($GETOPT -o nif -l no-symbols,in-place,force -n "$PROGRAM" -- "$@")"
-    local err=$?
-    eval set -- "$opts"
-    while true; do
-        case $1 in
-            -n|--no-symbols) symbols=""; shift ;;
-            -f|--force) force=1; shift ;;
-            -i|--in-place) inplace=1; shift ;;
-            --) shift; break ;;
-        esac
-    done
-
-    [[ $err -ne 0 || $# -lt 1 || ( $force -eq 1 && $inplace -eq 1 ) ]] \
-        && echo "Usage: $COMMAND pwfile [length] [-n,--no-symbols] [-i,--in-place | -f,--force]" \
-        && return
-
-    local path="$1"
-    local length="${2:-30}"    # default length 30
-    check_sneaky_paths "$path"
-    [[ ! $length =~ ^[0-9]+$ ]] \
-        && echo "Error: pass-length \"$length\" must be a number." \
-        && return
-
-    archive_unlock    # extract to $WORKDIR
-
-    mkdir -p "$WORKDIR/$(dirname "$path")"
-    local pwfile="$WORKDIR/$path"
-
-    if [[ $inplace -eq 0 && $force -eq 0 && -e $pwfile ]]; then
-        yesno "An entry already exists for $path. Overwrite it?" || return
-    fi
-
-    local pass="$(pwgen -s $symbols $length 1)"
-    [[ -n $pass ]] || return
-    if [[ $inplace -eq 0 ]]; then
-        cat <<< "$pass" > "$pwfile"
-    else
-        local pwfile_temp="${pwfile}.tmp.${RANDOM}.${RANDOM}.${RANDOM}.${RANDOM}.--"
-        cat "$pwfile" | sed $'1c \\\n'"$(sed 's/[\/&]/\\&/g' <<<"$pass")"$'\n' > "$pwfile_temp"
-        mv "$pwfile_temp" "$pwfile"
-        rm -f "$pwfile_temp"
-    fi
-    [[ -t 0 || -z $RUN_SHELL ]] && clip "$pass" "$path"
-
-    local verb="Add" ; [[ $inplace -eq 1 ]] && verb="Replace"
-    git_add_file "$pwfile" "$verb generated password for ${path}."
-
-    archive_lock      # cleanup $WORKDIR
-}
-
 cmd_delete() {
     local opts recursive="" force=0
     opts="$($GETOPT -o rf -l recursive,force -n "$PROGRAM" -- "$@")"
@@ -673,7 +621,7 @@ run_cmd() {
         grep)                    cmd_grep "$@" ;;
         set)                     cmd_set "$@" ;;
         edit)                    cmd_edit "$@" ;;
-        gen|generate)            cmd_generate "$@" ;;
+        #gen|generate)            cmd_generate "$@" ;;
         del|delete|rm|remove)    cmd_delete "$@" ;;
         mv|rename)               cmd_copy_move "move" "$@" ;;
         cp|copy)                 cmd_copy_move "copy" "$@" ;;
@@ -682,8 +630,7 @@ run_cmd() {
         keys|set-keys)           cmd_set_gpg_keys "$@" ;;
         export)                  cmd_export "$@" ;;
         import)                  cmd_import "$@" ;;
-        *)
-            try_ext_cmd $cmd "$@" || cmd_get "$cmd" ;;
+        *)                       try_ext_cmd $cmd "$@" ;;
     esac
 
     # cleanup the temporary workdir, if it is still there
@@ -758,8 +705,8 @@ try_ext_cmd() {
         return
     fi
 
-    # return false
-    return 1
+    # try to show the entry
+    cmd_get $cmd
 }
 
 debug() {
